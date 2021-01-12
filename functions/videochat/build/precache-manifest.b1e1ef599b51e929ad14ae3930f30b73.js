c61e39e11ebb743b789cc54a6c627d692da9b334
Avishai Vaisman <aviciiv@gmail.com>
2021-01-12 13:12:45 +0200

    marge express serving with firebase

---
+++ functions/videochat/build/precache-manifest.b1e1ef599b51e929ad14ae3930f30b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696682a1c359457169cc5b4771d61074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014e03aed9208ec80cf2",
+    "url": "/static/css/main.67bdd1e5.chunk.css"
+  },
+  {
+    "revision": "f8a5224f558b58284bdd",
+    "url": "/static/js/2.854240a1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.854240a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014e03aed9208ec80cf2",
+    "url": "/static/js/main.69bb119c.chunk.js"
+  },
+  {
+    "revision": "fcfe827e8c153e3bcda5",
+    "url": "/static/js/runtime-main.5ad136f7.js"
+  }
+]);
---
+++ functions/videochat/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // twilio: {
+  //   accountSid: process.env.TWILIO_ACCOUNT_SID,
+  //   apiKey: process.env.TWILIO_API_KEY,
+  //   apiSecret: process.env.TWILIO_API_SECRET
+  // }
+  twilio: {
+    accountSid: "AC4cb7baa083cde1e09298e7a9c41314e5",
+    apiKey: "SK4be5ac9f09897027d409a176124ed89b",
+    apiSecret: "jYXGhBwzsqizS7Xa7jXSprJZlYKZzXK5"
+  }
+};
